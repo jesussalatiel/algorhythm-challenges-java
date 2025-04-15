9e4ac7833c27e412e38741973a44e0376da8c163
Jesús Salatiel <jesussalatiel@hotmail.com>
2025-04-15 14:40:17 -0600

    feat(core): added readme.md

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@
  * For more details on building Java & JVM projects, please refer to https://docs.gradle.org/8.13/userguide/building_java_projects.html in the Gradle documentation.
  */
 
+
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
